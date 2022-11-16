792f6f1f8288de02dd9c278f7d88e6cc9893c957
Riccardo Rodini <113691534+Riccardo-Rodini@users.noreply.github.com>
2022-11-16 13:08:33 +0100

    Add files via upload

---
+++ contatorenpari.c
@@ -0,0 +1,22 @@
+/****************************************************************************************************
+*file: numpari.c                                                                                    *
+*autore: Rodini Riccardo                                                                            *
+*classe: 3H                                                                                         *
+*data: 16/11/22                                                                                     *
+****************************************************************************************************/
+#include<stdio.h>
+int main()
+{
+	int num;
+	int c=0;
+	
+	do{
+		printf("inserisci un numero\n");
+		scanf("%d",&num);
+		if(num%2==0)
+		   c++;
+	}while(num!=0);
+	
+      c--;
+	printf("hai inserito %d numeri pari",c);
+}
